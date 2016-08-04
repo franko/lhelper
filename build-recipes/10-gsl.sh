472397d92f473678d6c2fd375ae2afdcbe8a457f
Francesco Abbate <francesco.bbt@gmail.com>
2016-08-04 15:13:13 +0200

    Fix build recipe for GSL library

--- build-recipes/10-gsl.sh
+++ build-recipes/10-gsl.sh
@@ -8,4 +8,11 @@ CC="$CC_BASE" CFLAGS="$CFLAGS_BASE" ./configure "--prefix=$INSTALL_PREFIX"
 make
 make install
 
+PKG_NAME=gsl
+
+export WIN_INSTALL_PREFIX=${INSTALL_PREFIX/\/c\//c:\/}
+WIN_INSTALL_PREFIX=${WIN_INSTALL_PREFIX//\//\\\/}
+cat "${BUILD_DIR}/$PKG_NAME.pc.build" | sed "s/BUILD_PREFIX_DIR/$WIN_INSTALL_PREFIX/g" > "$PKG_NAME.pc"
+cp "$PKG_NAME.pc" "$INSTALL_PREFIX/lib/pkgconfig"
+
 touch "${BUILD_DIR}/${SCRIPT_NAME}.build-complete"
