04aaba007e21404db6e571ecb30bf5ff8d23f237
Francesco ABBATE <francesco abbate@st.com>
2017-08-18 17:46:54 +0200

    Update build recipe for FOX 1.6
    Name the package "fox" and embed pkgconfig template into the
    script.

--- build-recipes/10-fox16.sh
+++ build-recipes/10-fox16.sh
@@ -10,11 +10,31 @@ rm -fr "$INSTALL_PREFIX/include/fox-1.6"
 mkdir -p "$INSTALL_PREFIX/include/fox-1.6"
 cp include/*.h "$INSTALL_PREFIX/include/fox-1.6"
 
-PKG_NAME=fox16
+PKG_NAME=fox
 
-export WIN_INSTALL_PREFIX=${INSTALL_PREFIX/\/c\//c:\/}
-WIN_INSTALL_PREFIX=${WIN_INSTALL_PREFIX//\//\\\/}
-cat "$PKG_NAME.pc.build" | sed "s/BUILD_PREFIX_DIR/$WIN_INSTALL_PREFIX/g" > "$PKG_NAME.pc"
+WIN_INSTALL_PREFIX=${INSTALL_PREFIX/\/c\//c:\/}
+
+cat << EOF > ${PKG_NAME}.pc
+prefix=${WIN_INSTALL_PREFIX}
+exec_prefix=\${prefix}
+libdir=\${exec_prefix}/lib
+includedir=\${prefix}/include/fox-1.6
+LIBS=-lpthread
+X_LIBS=
+X_BASE_LIBS=-lcomctl32 -lwsock32 -lwinspool -lmpr -lgdi32 -limm32
+X_EXTRA_LIBS=
+GL_LIBS=
+FOX_LIBS=-lfox16
+
+Name: FOX
+Description: The FOX Toolkit
+URL: www.fox-toolkit.org
+Version: 1.6.52
+Libs: -L\${libdir} \${FOX_LIBS} \${X_BASE_LIBS}
+Libs.private: \${X_LIBS} \${X_BASE_LIBS} \${X_EXTRA_LIBS} \${GL_LIBS} \${LIBS}
+Cflags: -I\${includedir}
+EOF
+
 cp "$PKG_NAME.pc" "$INSTALL_PREFIX/lib/pkgconfig"
 
 touch "${BUILD_DIR}/${SCRIPT_NAME}.build-complete"
