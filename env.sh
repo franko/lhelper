c272fc2e1985a51b89469886fff94daac4a17c36
Francesco Abbate <francesco.bbt@gmail.com>
2017-12-04 13:46:01 +0100

    Use more aggressive optimizations flags

--- env.sh
+++ env.sh
@@ -3,13 +3,13 @@ then
     echo "Please provide the prefix directory argument"
 else
 
-CFLAGS_BASE='-O2 -fomit-frame-pointer'
-CXXFLAGS_BASE="-O2 -fomit-frame-pointer -ffast-math"
+CFLAGS_BASE='-O3 -fomit-frame-pointer -ffast-math'
+CXXFLAGS_BASE="-O3 -fomit-frame-pointer -ffast-math"
 INSTALL_PREFIX=$1
 
 export CC_BASE=gcc
 export CFLAGS_BASE=$CFLAGS_BASE
-export CFLAGS_AGG="$CXXFLAGS_BASE -ffast-math"
+export CFLAGS_AGG="$CXXFLAGS_BASE"
 export CXX_BASE=gcc
 export CXXFLAGS_BASE=$CXXFLAGS_BASE
 export CXXFLAGS_AGG="$CXXFLAGS_BASE -fno-exceptions -fno-rtti"
