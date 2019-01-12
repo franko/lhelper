0beaf3b8bbd2c47833885148eee8ec1066f6a18f
Francesco Abbate <francesco.bbt@gmail.com>
2019-01-12 19:34:48 +0100

    Improve FOX 1.6 recipe to work on Linux

--- recipes/fox16.sh
+++ recipes/fox16.sh
@@ -4,6 +4,14 @@ source "build-helper.sh"
 FOX_ARCHIVE_NAME=fox-1.6.57
 enter_remote_archive "${FOX_ARCHIVE_NAME}" "http://fox-toolkit.org/ftp/${FOX_ARCHIVE_NAME}.tar.gz" "${FOX_ARCHIVE_NAME}.tar.gz" "tar xzf ARCHIVE_FILENAME"
 
-CC="$CC_BASE" CFLAGS="$CFLAGS_FASTMATH" CXX="$CXX_BASE" CXXFLAGS="$CXXFLAGS_FASTMATH" ./configure --with-opengl=no --enable-release --disable-shared --with-x=no --disable-bz2lib --with-xft=no --prefix="${INSTALL_PREFIX}"
+if [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "mingw"* ]]; then
+    FOX_WITH_X=no
+    FOX_WITH_XFT=no
+else
+    FOX_WITH_X=yes
+    FOX_WITH_XFT=yes
+fi
+
+CC="$CC_BASE" CFLAGS="$CFLAGS_FASTMATH" CXX="$CXX_BASE" CXXFLAGS="$CXXFLAGS_FASTMATH" ./configure --with-opengl=no --enable-release --disable-shared --with-x=$FOX_WITH_X --disable-bz2lib --with-xft=$FOX_WITH_XFT --prefix="${INSTALL_PREFIX}"
 make
 make install
