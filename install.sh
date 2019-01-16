852521fec717287a36f81ef3414156f4561774e6
Francesco Abbate <francesco.bbt@gmail.com>
2019-01-16 14:24:39 +0100

    Avoid creating lhelper script on-the-fly
    The script exists now as an ordinary file. The lhelper's
    install directory is auto-detected using the $0 variable.

--- install.sh
+++ install.sh
@@ -23,35 +23,7 @@ cp env.sh "$DESTDIR/share/lhelper"
 cp build-helper.sh "$DESTDIR/share/lhelper"
 cp recipes/*.sh "$DESTDIR/share/lhelper/recipes"
 cp patch/*.patch "$DESTDIR/share/lhelper/patch"
-
-cat << EOF > "$DESTDIR/bin/lhelper"
-#!/usr/bin/env bash
-
-LHELPER_PREFIX="$PREFIX"
-
-if [ "\$#" -lt 1 ]; then
-    echo "Usage: \$0 <recipe-name>"
-    exit 1
-fi
-
-if [ -z \${LHELPER_ENV_NAME+x} ]; then
-    echo "error: no environment defined"
-    exit 1
-fi
-
-INSTALL_PREFIX="\$CMAKE_PREFIX_PATH"
-RECIPE="\$1"
-
-export LHELPER_WORKING_DIR="\$LHELPER_PREFIX/var/lib/lhelper"
-export LHELPER_DIR="\$LHELPER_PREFIX/share/lhelper"
-
-mkdir -p "\$LHELPER_WORKING_DIR/builds"
-mkdir -p "\$LHELPER_WORKING_DIR/repos"
-mkdir -p "\$LHELPER_WORKING_DIR/archives"
-
-source "\$LHELPER_DIR/env.sh"
-exec bash "\$LHELPER_DIR/recipes/\$RECIPE.sh"
-EOF
+cp lhelper "$DESTDIR/bin"
 
 chmod a+x "$DESTDIR/bin/lhelper"
 chmod a+x "$DESTDIR/bin/lhelper-env"
