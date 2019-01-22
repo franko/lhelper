839d31e863d78c4b79a23449dfa4df9dcebc7515
Francesco Abbate <francesco.bbt@gmail.com>
2019-01-22 14:33:26 +0100

    Use http with gnu ftp mirror for GSL library archive

--- recipes/gsl.sh
+++ recipes/gsl.sh
@@ -1,7 +1,7 @@
 set -e
 
 GSL_VERSION=gsl-2.1
-enter_remote_archive "$GSL_VERSION" "ftp://ftp.gnu.org/gnu/gsl/${GSL_VERSION}.tar.gz" "${GSL_VERSION}.tar.gz" "tar xzf ARCHIVE_FILENAME"
+enter_remote_archive "$GSL_VERSION" "http://ftpmirror.gnu.org/gsl/${GSL_VERSION}.tar.gz" "${GSL_VERSION}.tar.gz" "tar xzf ARCHIVE_FILENAME"
 
 CC="$CC_BASE" CFLAGS="$CFLAGS_BASE" ./configure --prefix="$WIN_INSTALL_PREFIX"
 make
