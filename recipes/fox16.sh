870fd51e2ebbf0e8003b70a337ebfb4a4022b5e9
Francesco Abbate <francesco.bbt@gmail.com>
2019-01-24 16:50:46 +0100

    Fix fox 1.6 recipe to include library path in pkgconfig

--- recipes/fox16.sh
+++ recipes/fox16.sh
@@ -2,6 +2,7 @@ set -e
 
 FOX_ARCHIVE_NAME=fox-1.6.57
 enter_remote_archive "${FOX_ARCHIVE_NAME}" "http://fox-toolkit.org/ftp/${FOX_ARCHIVE_NAME}.tar.gz" "${FOX_ARCHIVE_NAME}.tar.gz" "tar xzf ARCHIVE_FILENAME"
+inside_archive_apply_patch fox-1.6-configure-library-fix-2
 
 if [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "mingw"* ]]; then
     FOX_WITH_X=no
