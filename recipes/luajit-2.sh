4d7b3d445a9e67047cb878642f22a2f5ddff5265
Francesco Abbate <francesco.bbt@gmail.com>
2019-02-06 11:56:40 +0100

    Fix luajit-2 recipe

--- recipes/luajit-2.sh
+++ recipes/luajit-2.sh
@@ -3,8 +3,5 @@ LUAJIT_PACKAGE="LuaJIT-2.0.5"
 LUAJIT_DIR=${LUAJIT_PACKAGE}
 enter_remote_archive "${LUAJIT_DIR}" "http://luajit.org/download/${LUAJIT_PACKAGE}.tar.gz" "${LUAJIT_PACKAGE}.tar.gz" "tar xzf ARCHIVE_FILENAME"
 
-cat Makefile | sed "s/^export PREFIX=.*/export PREFIX= ${WIN_INSTALL_PREFIX}/g" > "Makefile.tmp" && mv Makefile.tmp Makefile
-export CC=$CC_BASE
-CC=$CC_BASE PREFIX=$INSTALL_PREFIX make
-PREFIX=$INSTALL_PREFIX make install
-cp src/lua51.dll "$INSTALL_PREFIX/bin"
+CC=$CC_BASE make PREFIX="$INSTALL_PREFIX"
+make PREFIX="$INSTALL_PREFIX" install
