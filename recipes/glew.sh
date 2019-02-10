b36d873323efe73ac725f924d0e72e7a5f41a8c1
Francesco Abbate <francesco.bbt@gmail.com>
2019-02-10 22:31:37 +0100

    Fix flew recipe

--- recipes/glew.sh
+++ recipes/glew.sh
@@ -1,15 +1,10 @@
-set -e
 enter_git_repository glew https://github.com/nigels-com/glew.git master
 
 echo "Applying patch glew-remove-nostdlib-flag"
 inside_git_apply_patch "glew-remove-nostdlib-flag"
 
-export CC="$CC_BASE"
-export CFLAGS="$CFLAGS_FASTMATH"
-export CXX="$CXX_BASE"
-export CXXFLAGS="$CXXFLAGS_FASTMATH"
 export GLEW_DEST="${INSTALL_PREFIX}"
 
-make extensions
+make extentions
 make all
 make install.all
