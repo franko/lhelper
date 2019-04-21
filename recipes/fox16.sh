62a6e010e94625019673f9e4f771ca68a769fcad
Francesco Abbate <francesco.bbt@gmail.com>
2019-04-21 17:06:23 +0200

    Use Meson based build for FOX library

--- recipes/fox16.sh
+++ recipes/fox16.sh
@@ -1,13 +1,4 @@
-FOX_ARCHIVE_NAME=fox-1.6.57
+fox_version=1.6.57
+enter_git_repository fox-1.6 https://github.com/franko/fox-1.6.git "v${fox_version}"
+build_and_install meson -Dopengl=false
 
-if [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "mingw"* ]]; then
-    FOX_WITH_X=no
-    FOX_WITH_XFT=no
-else
-    FOX_WITH_X=yes
-    FOX_WITH_XFT=yes
-fi
-
-enter_remote_archive "${FOX_ARCHIVE_NAME}" "http://fox-toolkit.org/ftp/${FOX_ARCHIVE_NAME}.tar.gz" "${FOX_ARCHIVE_NAME}.tar.gz" "tar xzf ARCHIVE_FILENAME"
-inside_archive_apply_patch fox-1.6-configure-library-fix-2
-build_and_install configure --with-opengl=no --disable-shared --with-x=$FOX_WITH_X --disable-bz2lib --with-xft=$FOX_WITH_XFT
