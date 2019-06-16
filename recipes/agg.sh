02b73c5a9e191aecf52366bac9ea293e911edf04
Francesco Abbate <francesco.bbt@gmail.com>
2019-06-16 18:22:57 +0200

    Remove pkg-config file generation from agg library's recipe
    No longer needed since the github based build of AGG already includes
    the pkg-config file.

--- recipes/agg.sh
+++ recipes/agg.sh
@@ -1,22 +1,2 @@
 enter_git_repository agg https://github.com/franko/agg.git master
 build_and_install meson
-
-PKG_NAME=libagg
-
-# Warning, since the 'EOF' below is unquoted shell variables substitutions
-# will be done on the text body. The '$' should be therefore escaped to
-# avoid shell substitution when needed.
-cat << EOF > $PKG_NAME.pc
-prefix=${WIN_INSTALL_PREFIX}
-libdir=\${prefix}/lib
-includedir=\${prefix}/include/agg2
-
-Name: libagg
-Description: Anti-grain library
-Version: 2.5.0
-
-Libs: -L\${libdir} -lagg -lm
-Cflags: -I\${includedir}
-EOF
-
-cp "$PKG_NAME.pc" "$INSTALL_PREFIX/lib/pkgconfig"
