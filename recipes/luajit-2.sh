3938b17e8736f6e3db108de16ea6fa0dc2d87fd4
Francesco Abbate <francesco.bbt@gmail.com>
2019-06-25 11:05:05 +0200

    Replace luajit recipe with meson build from github
    Using now https://github.com/franko/luajit.git with the meson
    build.
    
    Renamed the recipe 'luajit' instead on 'luajit-2' to be coherent
    with pkg-config package name.

--- recipes/luajit-2.sh
+++
@@ -1,7 +0,0 @@
-LUAJIT_PACKAGE="LuaJIT-2.0.5"
-LUAJIT_DIR=${LUAJIT_PACKAGE}
-enter_remote_archive "${LUAJIT_DIR}" "http://luajit.org/download/${LUAJIT_PACKAGE}.tar.gz" "${LUAJIT_PACKAGE}.tar.gz" "tar xzf ARCHIVE_FILENAME"
-inside_archive_apply_patch "luajit-2.0-windows-install-fix"
-
-make PREFIX="$INSTALL_PREFIX"
-make PREFIX="$INSTALL_PREFIX" install
---
+++ recipes/luajit.sh
@@ -0,0 +1,2 @@
+enter_git_repository luajit https://github.com/franko/luajit.git 2.0.5-franko
+build_and_install meson
