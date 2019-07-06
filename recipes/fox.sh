e7e21e2cad747a3d1a20bfed4b78da1e1ca53b74
Francesco Abbate <francesco.bbt@gmail.com>
2019-07-06 21:32:20 +0200

    Build only static library for FOX library and disable apps

--- recipes/fox.sh
+++ recipes/fox.sh
@@ -1,4 +1,4 @@
-fox_version=1.6.57
+fox_version="1.6.57-franko1"
 enter_git_repository fox-1.6 https://github.com/franko/fox-1.6.git "v${fox_version}"
-build_and_install meson -Dopengl=false
+build_and_install meson -Ddefault_library=static -Dapps=false -Dopengl=false
 
