79a06a3cd66d42617dd47b4bca6c73f73aee50c1
Francesco Abbate <francesco.bbt@gmail.com>
2019-11-03 17:46:01 +0100

    Update freetype library recipe
    To be tested on windows.

--- recipes/freetype2.sh
+++ recipes/freetype2.sh
@@ -1,25 +1,3 @@
-set -e
-FREETYPE_VERSION="2.7"
+FREETYPE_VERSION="2.10.1"
 enter_remote_archive "freetype-${FREETYPE_VERSION}" "http://download.savannah.gnu.org/releases/freetype/freetype-${FREETYPE_VERSION}.tar.gz" "freetype-${FREETYPE_VERSION}.tar.gz" "tar xzf ARCHIVE_FILENAME"
-
-mkdir build && cd build
-cmake -G "Ninja" -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -DWITH_ZLIB=OFF -DWITH_BZip2=OFF -DWITH_PNG=OFF -DCMAKE_DISABLE_FIND_PACKAGE_HarfBuzz=TRUE ..
-cmake --build .
-cmake --build . --target install
-
-PKG_NAME=freetype2
-
-# Warning, since the 'EOF' below in unquoted shell variables substitutions
-# will be done on the text body. The '$' should be therefore escaped to
-# avoid shell substitution when needed.
-cat << EOF > $PKG_NAME.pc
-prefix=${WIN_INSTALL_PREFIX}
-
-Name: Freetype2
-Description: Freetype2 library
-Version: ${FREETYPE_VERSION}
-Libs: -L\${prefix}/lib -lfreetype
-Cflags: -I\${prefix}/include/freetype2
-EOF
-
-install_pkgconfig_file "${PKG_NAME}.pc"
+build_and_install configure --disable-shared --disable-largefile --disable-mmap --with-zlib=no --with-bzip2=no --with-png=no --with-harfbuzz=no
