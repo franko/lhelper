ce30d88eba78cd6c412578d4bb395ed9291e93d7
Francesco Abbate <francesco.bbt@gmail.com>
2019-11-04 23:36:18 +0100

    Re-estabilish and fix the recipe for freetype2

--- recipes/freetype2.sh
+++ recipes/freetype2.sh
@@ -1,3 +1,30 @@
-FREETYPE_VERSION="2.10.1"
+FREETYPE_VERSION="2.7"
 enter_remote_archive "freetype-${FREETYPE_VERSION}" "http://download.savannah.gnu.org/releases/freetype/freetype-${FREETYPE_VERSION}.tar.gz" "freetype-${FREETYPE_VERSION}.tar.gz" "tar xzf ARCHIVE_FILENAME"
-build_and_install configure --disable-shared --disable-largefile --disable-mmap --with-zlib=no --with-bzip2=no --with-png=no --with-harfbuzz=no
+
+mkdir build && cd build
+cmake -G "Ninja" -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -DWITH_ZLIB=OFF -DWITH_BZip2=OFF -DWITH_PNG=OFF -DCMAKE_DISABLE_FIND_PACKAGE_HarfBuzz=TRUE ..
+cmake --build .
+cmake --build . --target install
+
+if [ "$BUILD_TYPE" == "Debug" ]; then
+    ftlibname=freetyped
+else
+    ftlibname=freetype
+fi
+
+PKG_NAME=freetype2
+
+# Warning, since the 'EOF' below in unquoted shell variables substitutions
+# will be done on the text body. The '$' should be therefore escaped to
+# avoid shell substitution when needed.
+cat << EOF > $PKG_NAME.pc
+prefix=${WIN_INSTALL_PREFIX}
+
+Name: Freetype2
+Description: Freetype2 library
+Version: ${FREETYPE_VERSION}
+Libs: -L\${prefix}/lib -l$ftlibname
+Cflags: -I\${prefix}/include/freetype2
+EOF
+
+install_pkgconfig_file "${PKG_NAME}.pc"
