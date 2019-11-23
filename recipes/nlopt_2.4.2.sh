8dbf63e820d65a6e25abc5d3e98c92b569318c4f
Francesco Abbate <francesco.bbt@gmail.com>
2019-11-23 23:13:09 +0100

    Fix error in nlopt recope

--- recipes/nlopt_2.4.2.sh
+++ recipes/nlopt_2.4.2.sh
@@ -1,3 +1,3 @@
-NLOPT_VERSION="nlopt-$2"
-enter_remote_archive "$NLOPT_VERSION" "https://github.com/stevengj/nlopt/releases/download/nlopt-$NLOPT_VERSION/$NLOPT_VERSION.tar.gz" "$NLOPT_VERSION.tar.gz" "tar xzf ARCHIVE_FILENAME"
+NLOPT_VERSION="$2"
+enter_remote_archive "nlopt-$NLOPT_VERSION" "https://github.com/stevengj/nlopt/releases/download/nlopt-$NLOPT_VERSION/nlopt-$NLOPT_VERSION.tar.gz" "nlopt-$NLOPT_VERSION.tar.gz" "tar xzf ARCHIVE_FILENAME"
 build_and_install configure --disable-shared
