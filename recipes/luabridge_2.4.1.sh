27955315e0c9c982e892429440a6adbb4203685e
Francesco Abbate <francesco.bbt@gmail.com>
2020-01-25 23:20:47 +0100

    Add recipe for LuaBridge

---
+++ recipes/luabridge_2.4.1.sh
@@ -0,0 +1,11 @@
+luabridge_install () {
+    mkdir build && pushd build
+    cmake -G "Ninja" -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" ..
+    cmake --build .
+    popd
+    mkdir -p "$INSTALL_PREFIX/include"
+    cp -r Source/LuaBridge "$INSTALL_PREFIX/include"
+}
+
+enter_git_repository LuaBridge https://github.com/vinniefalco/LuaBridge.git "$2" --recurse-submodules
+luabridge_install
