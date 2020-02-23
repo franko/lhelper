ef0575e3b54db395cfbd2ea2014eb6c8d7ddf338
Francesco Abbate <francesco.bbt@gmail.com>
2020-02-23 12:10:58 +0100

    Testing new wxwidgets recipe

--- recipes/wxwidgets_3.1.0.sh
+++
@@ -1,9 +0,0 @@
-WXWIDGETS_PACKAGE="wxWidgets-$2"
-enter_remote_archive "$WXWIDGETS_PACKAGE" "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/$WXWIDGETS_PACKAGE.tar.bz2" "$WXWIDGETS_PACKAGE.tar.bz2" "tar xjf ARCHIVE_FILENAME"
-build_and_install configure
-
-# replace string like:
-# BK_DEPS = /c/fra/src/wxwidgets/bk-deps
-# with:
-# BK_DEPS = c:/fra/src/wxwidgets/bk-deps
-# find . -name 'Makefile' -exec sed -i 's/= *\/c\//= c:\//g' '{}' \;
---
+++ recipes/wxwidgets_3.1.3.sh
@@ -0,0 +1,27 @@
+WXWIDGETS_PACKAGE="wxWidgets-$2"
+enter_remote_archive "$WXWIDGETS_PACKAGE" "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.3/$WXWIDGETS_PACKAGE.tar.bz2" "$WXWIDGETS_PACKAGE.tar.bz2" "tar xjf ARCHIVE_FILENAME"
+
+if [[ "$OSTYPE" == "linux"* ]]; then
+    BACKEND_OPTIONS=(--with-gtk=3)
+fi
+
+# Normally we may use --enable-no_exceptions but as 3.1.3 is broken because Scintilla
+# requires exceptions.
+
+# If SVG is enable PNG library is required.
+
+# wx-config will print the options enabled.
+
+# stc could be disabled (scintilla based styled text)
+
+# Disable language localisation maybe.
+
+X_OPTIONS=(--disable-{svg,webkit,webview,unicode,compat30} --enable-no_rtti --without-{libxpm,libiconv,gnomevfs,libnotify,opengl,dmalloc,sdl,regex,zlib,expat,libpng,libjpeg,libtiff})
+
+build_and_install configure "${X_OPTIONS[@]}" "${BACKEND_OPTIONS[@]}"
+
+# replace string like:
+# BK_DEPS = /c/fra/src/wxwidgets/bk-deps
+# with:
+# BK_DEPS = c:/fra/src/wxwidgets/bk-deps
+# find . -name 'Makefile' -exec sed -i 's/= *\/c\//= c:\//g' '{}' \;
