12e8d827b807483c2e1ddf3b2ff8177eb829e6ba
Francesco Abbate <francesco.bbt@gmail.com>
2022-04-14 23:56:49 +0200

    Make the CPU flags operational

--- create-env.sh
+++ create-env.sh
@@ -3,6 +3,12 @@
 
 INSTALL_PREFIX="$2"
 _build_type="${3:-Release}"
+arch_target="$4"
+
+IFS=$':' read -a arch_arr <<< "$arch_target"
+export CPU_TYPE="${arch_arr[0]}"
+export CPU_TARGET="${arch_arr[1]}"
+# TODO: we should consider downgrading the CPU_TARGET if the compiler doesn't support it.
 
 if [[ $_build_type != "Release" && $_build_type != "Debug" ]]; then
     echo "Build type should be either Release or Debug, abort."
@@ -11,6 +17,51 @@ fi
 
 if [ -z ${CC+x} ]; then CC="gcc"; fi
 if [ -z ${CXX+x} ]; then CXX="g++"; fi
+
+# https://gcc.gnu.org/onlinedocs/gcc/x86-Options.html
+# https://en.wikipedia.org/wiki/List_of_Intel_CPU_microarchitectures#x86_microarchitectures
+# https://en.wikipedia.org/wiki/X86-64#Microarchitecture_levels
+# https://en.wikipedia.org/wiki/X86#Chronology
+
+# x86-64 => INTEL: prescott (ok GCC), AMD: (GCC) k8, opteron, athlon64, athlon-fx, includes MMX, SSE, SSE2
+# x86-64-v2 => nehalem (ok GCC), AMD: jaguar (bdver1 for GCC), includes SSE3, SSE4_1, SSE4_2, SSSE3
+# x86-64-v3 => haswell (ok GCC), AMD: (bdver4 for GCC), includes AVX, AVX2
+# x86-64-v4 => AVX512
+
+# Clang available march: llc -march=x86-64 -mattr=help
+
+# We do not cover all the OpenBLAS targets for x86/x86-64
+# FIXME: ARM is not covered
+# meaning: "architecture", "OpenBLAS name", "uname -m", "c compiler arch name", "c compiler arch fallback name 1 et 2"
+# "additional flags"
+known_cpu_spec=(
+    "x86    p2          x86    pentium2       i686      i686     -mfpmath=sse,-msse"
+    "x86    prescott    x86    prescott       pentium4  i686     -mfpmath=sse,-msse2"
+    "x86    nehalem     x86    nehalem        pentium4  i686     -mfpmath=sse,-msse2"
+    "x86    haswell     x86    haswell        pentium4  i686     -mfpmath=sse,-msse2"
+    "x86-64 prescott    x86_64 prescott       x86-64    x86-64"
+    "x86-64 athlon      x86_64 athlon         x86-64    x86-64"
+    "x86-64 opteron     x86_64 opteron        x86-64    x86-64"
+    "x86-64 barcelona   x86_64 barcelona      x86-64    x86-64"
+    "x86-64 bulldozer   x86_64 bdver1         x86-64-v2 x86-64"
+    "x86-64 jaguar      x86_64 bdver3         x86-64-v2 x86-64" # verifier
+    "x86-64 nehalem     x86_64 nehalem        x86-64-v2 x86-64"
+    "x86-64 sandybridge x86_64 sandybridge    x86-64-v2 x86-64"
+    "x86-64 haswell     x86_64 haswell        x86-64-v3 x86-64"
+    "x86-64 excavator   x86_64 bdver4         x86-64-v3 x86-64"
+    "x86-64 skylakex    x86_64 skylake-avx512 x86-64-v4 x86-64" # verifier
+)
+
+cpu_mflag=""
+if [[ $CPU_TYPE == x86 ]]; then
+    cpu_mflag="-m32"
+fi
+for line in "${known_cpu_spec[@]}"; do
+    read -a line_a <<< "$line"
+    if [[ "${line_a[0]}:${line_a[1]}" == "$CPU_TYPE:$CPU_TARGET" ]]; then
+        CPU_CFLAGS="$cpu_mflag -march=${line_a[3]} ${line_a[6]//,/ }"
+    fi
+done
 
 # Take a format and a list or aguments. Format each argument with
 # the given format using printf ang join all strings.
@@ -66,11 +117,15 @@ cat << _EOF_ > "${INSTALL_PREFIX}/bin/lhelper-config"
 # are automatically added by cmake or meson depending on
 # the BUILD_TYPE variable.
 
-export CC="$CC"
-export CXX="$CXX"
+export CC_BARE="$CC"
+export CXX_BARE="$CXX"
+export CC="$CC $CPU_CFLAGS"
+export CXX="$CXX $CPU_CFLAGS"
 export CFLAGS="$CFLAGS"
 export CXXFLAGS="$CXXFLAGS"
 export LDFLAGS="$LDFLAGS"
+export CPU_TYPE="$CPU_TYPE"
+export CPU_TARGET="$CPU_TARGET"
 
 # Can be Release or Debug
 export BUILD_TYPE="$_build_type"
